f7e34387adfbb713b9967c35bdffa98b2bd926bd
Anthony <bondolo90@gmail.com>
2023-10-29 08:41:08 -0700

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,8 +7,25 @@ from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def api_status():
+    def api_status():
     """return the status of the app
     """
     response = {'status': 'OK'}
     return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+    def get_stats():
+    '''
+    Retrieves the number of each objects by type.
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
+
