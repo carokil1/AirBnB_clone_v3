4bb8fdb5e71ee70ca56ae8f155222116e09451b6
Anthony <bondolo90@gmail.com>
2023-10-29 10:40:34 -0700

    changing Except in the filestorage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,9 +55,9 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except Exception as e:
+        except Exception:
             # handle the error or perhaps log it
-            print(f"An error occurred: {e}")
+            pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
